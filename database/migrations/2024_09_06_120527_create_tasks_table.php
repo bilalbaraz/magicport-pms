181e5c40af92e73cfb818157bd70b217e62d78bd
Bilal Baraz <bilalbaraz@windowslive.com>
2024-09-06 17:03:17 +0300

    Add softDeletes to tasks

--- database/migrations/2024_09_06_120527_create_tasks_table.php
+++ database/migrations/2024_09_06_120527_create_tasks_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->longText('description')->nullable();
             $table->enum('status', [TaskStatusEnums::TO_DO, TaskStatusEnums::IN_PROGRESS, TaskStatusEnums::DONE]);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
